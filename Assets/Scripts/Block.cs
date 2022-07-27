b45e860d9d961e7d418322dc9d4fcb996245febc
yevhenkr <vasilenkoevgen88@gmail.com>
2022-07-27 11:42:35 +0300

    Minor fix

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,25 +1,33 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-    private int healthPoint;
+    public event Action EventDestroyBlock;
+    [SerializeField] private int healthPoint;
 
     public void SetHealthValue(int health)
     {
         healthPoint = health;
     }
+
     public void SetSprite(Sprite sprite)
     {
         this.gameObject.GetComponent<SpriteRenderer>().sprite = sprite;
     }
-    
+
     void OnCollisionEnter2D(Collision2D col)
     {
-        ReturnToPool();
+        healthPoint--;
+        if (healthPoint == 0)
+        {
+            ReturnToPool();
+        }
     }
 
     public void ReturnToPool()
     {
+        EventDestroyBlock?.Invoke();
         gameObject.SetActive(false);
     }
 }
