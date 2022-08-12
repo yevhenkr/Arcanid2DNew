226b5084a5eee3f0ab76a09f0022f79a2a5568c7
yevhenkr <vasilenkoevgen88@gmail.com>
2022-08-12 21:20:11 +0300

    Added desin destroy block

--- Assets/Scripts/Blocks/Block.cs
+++ Assets/Scripts/Blocks/Block.cs
@@ -5,26 +5,40 @@ public class Block : MonoBehaviour
 {
     public event Action EventDestroyBlock;
     public event Action<BlockData> BallData;
-    [SerializeField] private int healthPoints;
+    [SerializeField] private float healthPoints;
+    private Color color;
+    private float maxHealth;
 
-    void OnCollisionEnter2D(Collision2D col)
+    private void Awake()
+    {
+        color = gameObject.GetComponent<SpriteRenderer>().color;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision2D)
     {
         healthPoints--;
-        if (healthPoints == 0)
+        if (healthPoints <= 0)
         {
             ReturnToPool();
+        }
+        else
+        {
+            color.a = healthPoints / maxHealth;
+            gameObject.GetComponent<SpriteRenderer>().color = color;
         }
     }
 
     public void ReturnToPool()
     {
         EventDestroyBlock?.Invoke();
+        color.a = 255;
         gameObject.SetActive(false);
     }
 
     public void SetHealthValue(int health)
     {
         healthPoints = health;
+        maxHealth = healthPoints;
     }
 
     public void SetSprite(Sprite sprite)
